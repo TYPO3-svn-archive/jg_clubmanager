d7e1630e66e30d07620f5e5a6cc08bc7757864f4
gebauer <gebauer@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-26 14:23:34 +0000

    First upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jg_clubmanager/trunk@25843 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "jg_clubmanager"
+#
+# Auto generated 21-10-2009 14:24
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Club Manager',
+	'description' => 'Club Manger Description',
+	'category' => 'be',
+	'author' => 'Jan Gebauer',
+	'author_email' => 'mail@jan-gebauer.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"c7cf";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"b4f1";s:19:"doc/wizard_form.dat";s:4:"bc4c";s:20:"doc/wizard_form.html";s:4:"69a8";s:13:"mod1/conf.php";s:4:"84bf";s:14:"mod1/index.php";s:4:"1429";s:18:"mod1/locallang.xml";s:4:"473f";s:22:"mod1/locallang_mod.xml";s:4:"2619";s:22:"mod1/mod_template.html";s:4:"0db4";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+);
+
+?>
